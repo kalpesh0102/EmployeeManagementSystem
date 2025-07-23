3441d0a606e2ef9280762c4dd7e3b99332ff4aef
kalpesh vibhute <kalpeshvibhute13@gmail.com>
2025-07-23 18:13:08 +0530

    Add files via upload

---
+++ Conn.java
@@ -0,0 +1,19 @@
+package EmployeeManagement;
+
+import java.sql.*;
+
+public class Conn {
+    
+    Connection c;
+    Statement s;
+
+    public Conn () {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c = DriverManager.getConnection("jdbc:mysql:///employeemanagementsystem", "root", "Kalpesh@09");
+            s = c.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
